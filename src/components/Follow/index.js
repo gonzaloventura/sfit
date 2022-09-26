70ee0e58d82c7f4ac0fbebff080623f12e71e334
Gonzalo Ventura <gonzalogventura@gmail.com>
2022-09-26 00:01:49 -0300

    update

--- src/components/Follow/index.js
+++ src/components/Follow/index.js
@@ -6,24 +6,24 @@ const Follow = () => {
   return (
     <>
     <div className='cuenta__follow'>
-        <img className='brand' src={require("../../" + data[0].logo)} />
-        <a target={'_blank'} href={'https://www.instagram.com/chacinados_tacural_srl/'}>Seguir en instagram</a>
+        <img className='brand' src={require("../../" + data[0].logo)} alt={data[0].marca} />
+        <a target={'_blank'} rel="noreferrer" href={'https://www.instagram.com/chacinados_tacural_srl/'}>Seguir en instagram</a>
     </div>
     <div className='cuenta__follow'>
-        <img className='brand' src={require("../../" + data[1].logo)} />
-        <a target={'_blank'} href={'https://www.instagram.com/milkautarg/'}>Seguir en instagram</a>
+        <img className='brand' src={require("../../" + data[1].logo)} alt={data[1].marca} />
+        <a target={'_blank'} rel="noreferrer" href={'https://www.instagram.com/milkautarg/'}>Seguir en instagram</a>
     </div>
     <div className='cuenta__follow'>
-        <img className='brand' src={require("../../" + data[2].logo)} />
-        <a target={'_blank'} href={'https://www.instagram.com/alfajoresmerengo/'}>Seguir en instagram</a>
+        <img className='brand' src={require("../../" + data[2].logo)} alt={data[2].marca}/>
+        <a target={'_blank'} rel="noreferrer" href={'https://www.instagram.com/alfajoresmerengo/'}>Seguir en instagram</a>
     </div>
     <div className='cuenta__follow'>
-        <img className='brand' src={require("../../" + data[3].logo)} />
-        <a target={'_blank'} href={'https://www.instagram.com/cervezasantafe/'}>Seguir en instagram</a>
+        <img className='brand' src={require("../../" + data[3].logo)} alt={data[3].marca}/>
+        <a target={'_blank'} rel="noreferrer" href={'https://www.instagram.com/cervezasantafe/'}>Seguir en instagram</a>
     </div>
     <div className='cuenta__follow'>
-        <img className='brand' src={require("../../" + data[4].logo)} />
-        <a target={'_blank'} href={'https://www.instagram.com/santafecapitalok/'}>Seguir en instagram</a>
+        <img className='brand' src={require("../../" + data[4].logo)} alt={data[4].marca}/>
+        <a target={'_blank'} rel="noreferrer" href={'https://www.instagram.com/santafecapitalok/'}>Seguir en instagram</a>
     </div>
     </>
   )
