736f2392e250bb384b7ee62439ed7d676052e2e5
Gonzalo Ventura <gonzaloventura@MacBook-Pro-de-Gonzalo.local>
2022-12-02 20:29:52 -0300

    sin firebase

--- src/components/LuckyWheel/index.js
+++ src/components/LuckyWheel/index.js
@@ -11,44 +11,92 @@ const LuckyWheel = ({nombre, email}) => {
   const [chances, setChances] = useState(1);   
   const [style, setStyle] = useState(0);
   const [elGanadorEs, setElGanadorEs] = useState();
-  const [data, setData] = useState([]);
+  const [data, setData] = useState([
+    {
+    disponibles: 100,
+    info: "Acércate a nuestro stand para retirarlo.",
+    logo: "assets/images/brands/01.png",
+    marca: "Francelina Espinoza",
+    premio: "Ganaste",
+    probabilidad: 0,
+    rotacion_max: 358,
+    rotacion_min: 302
+  },
+  {
+    disponibles: 100,
+    info: "Acércate a nuestro stand para retirarlo.",
+    logo: "assets/images/brands/02.png",
+    marca: "Cesar Flodata",
+    premio: "Ganaste",
+    probabilidad: 0,
+    rotacion_max: 298,
+    rotacion_min: 242
+  },
+  {
+    disponibles: 100,
+    info: "Acércate a nuestro stand para retirarlo.",
+    logo: "assets/images/brands/03.png",
+    marca: "Walter Yanho",
+    premio: "Ganaste",
+    probabilidad: 0,
+    rotacion_max: 238,
+    rotacion_min: 182
+  },{
+    disponibles: 100,
+    info: "Acércate a nuestro stand para retirarlo.",
+    logo: "assets/images/brands/04.png",
+    marca: "Victoria Bavosi",
+    premio: "Ganaste",
+    probabilidad: 0,
+    rotacion_max: 178,
+    rotacion_min: 122
+  },{
+    disponibles: 100,
+    info: "Acércate a nuestro stand para retirarlo.",
+    logo: "assets/images/brands/05.png",
+    marca: "Madeleine Orihuela",
+    premio: "Ganaste",
+    probabilidad: 0,
+    rotacion_max: 118,
+    rotacion_min: 62
+  },{
+    disponibles: 100,
+    info: "Acércate a nuestro stand para retirarlo.",
+    logo: "assets/images/brands/06.png",
+    marca: "Jaime Miranda Vega",
+    premio: "Ganaste",
+    probabilidad: 0,
+    rotacion_max: 58,
+    rotacion_min: 2
+  }
+]);
 
   const [success, setSuccess] = useState()
 
   const [order, setOrder] = useState({});
   const [userData, setUserData] = useState({});
 
-  const getData = async () => {
-    const dataCollection = collection(db, 'data')
-    const dataSnapshot = await getDocs(dataCollection)
-    const dataList = dataSnapshot.docs.map( (doc) => {
-        let sponsor = doc.data()
-        sponsor.id = doc.id
-        return sponsor
-    })
-    return dataList;
-  }
 
   useEffect(() => {
-    getData()
-    .then((res) => {
-      setData(res);
-      let randomNumber = obtenerValor(1,13);
-      if (randomNumber === 1 && res[1].disponibles > 0){
+    giroRandom()
+}, [])
+
+const giroRandom = () => {
+  let randomNumber = obtenerValor(1,13);
+      if (randomNumber === 1 && data[1].disponibles > 0){
         setElGanadorEs("Francelina Espinoza");
-      } else if (randomNumber === 2 && res[2].disponibles > 0){
-        setElGanadorEs("Cesar Flores");
-      }  else if ((randomNumber === 3 || randomNumber === 6 || randomNumber === 7) && res[2].disponibles > 0){
+      } else if (randomNumber === 2 && data[2].disponibles > 0){
+        setElGanadorEs("Cesar Flodata");
+      }  else if ((randomNumber === 3 || randomNumber === 6 || randomNumber === 7) && data[2].disponibles > 0){
         setElGanadorEs("Walter Yanho");
-      }  else if ((randomNumber === 4 || randomNumber === 8 || randomNumber === 9) && res[3].disponibles > 0){
+      }  else if ((randomNumber === 4 || randomNumber === 8 || randomNumber === 9) && data[3].disponibles > 0){
         setElGanadorEs("Victoria Bavosi");
-      }  else if ((randomNumber === 5 || randomNumber === 10 || randomNumber === 11) && res[4].disponibles > 0){
+      }  else if ((randomNumber === 5 || randomNumber === 10 || randomNumber === 11) && data[4].disponibles > 0){
         setElGanadorEs("Madeleine Orihuela");
       } else if (randomNumber === 12 || randomNumber === 13) {
         setElGanadorEs("Jaime Miranda Vega");
-      }
-    }); 
-}, [chances, setChances, elGanadorEs, success, setElGanadorEs, data])
+      } 
+}
 
 
   useEffect(() => {
@@ -73,22 +121,16 @@ const LuckyWheel = ({nombre, email}) => {
   function quienGana(marca){
     switch (marca){
       case "Francelina Espinoza":
-        updateDoc(doc(db, 'data', "0"), {disponibles: data[0].disponibles - 1})
         return(obtenerValor(data[0].rotacion_min, data[0].rotacion_max));
-      case "Cesar Flores":
-        updateDoc(doc(db, 'data', "1"), {disponibles: data[1].disponibles - 1});
+      case "Cesar Flodata":
         return(obtenerValor(data[1].rotacion_min, data[1].rotacion_max));
       case "Walter Yanho":
-        updateDoc(doc(db, 'data', "2"), {disponibles: data[2].disponibles - 1});
         return(obtenerValor(data[2].rotacion_min, data[2].rotacion_max));
       case "Victoria Bavosi":
-        updateDoc(doc(db, 'data', "3"), {disponibles: data[3].disponibles - 1});
         return(obtenerValor(data[3].rotacion_min, data[3].rotacion_max));
       case "Madeleine Orihuela":
-        updateDoc(doc(db, 'data', "4"), {disponibles: data[4].disponibles - 1});
         return(obtenerValor(data[4].rotacion_min, data[4].rotacion_max));
       case "Jaime Miranda Vega":
-        updateDoc(doc(db, 'data', "5"), {disponibles: data[5].disponibles - 1})
         return(obtenerValor(data[5].rotacion_min, data[5].rotacion_max));
       default:
         console.log("error");
@@ -100,6 +142,7 @@ const LuckyWheel = ({nombre, email}) => {
   }
 
   const validacionPrevia = () => {
+      giroRandom()
       startRotation();
   }
   
@@ -114,7 +157,6 @@ const LuckyWheel = ({nombre, email}) => {
         startVelocity: 25,
         spread: 120,
         });
-        pushData({...userData, premio: buscarEnData(elGanadorEs).marca});
     }, 4000);
     setTimeout(() => {
       Swal.fire({
@@ -122,13 +164,6 @@ const LuckyWheel = ({nombre, email}) => {
         text: buscarEnData(elGanadorEs).marca, 
         confirmButtonColor: '#6CACE4'
       })}, 5500);
-  }
-
-  const pushData = async (newOrder) => {
-    const collectionOrder = collection(db, 'entregados')
-    const orderDoc = await addDoc(collectionOrder, newOrder)
-    setSuccess(orderDoc.id)
-    console.log('ORDEN GENERADA', orderDoc)
   }
   
   const seQuedoSinChances = () => {
